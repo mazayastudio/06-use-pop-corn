99550533faf4c92df2ce79e20e601d0fce399abd
sayudhalw <mazaya.studio@outlook.com>
2026-04-22 06:46:03 +0700

    Refactor App component: consolidate Box components and remove redundant WatchBox logic.

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -59,10 +60,15 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main movies={movies}>
-        <ListBox movies={movies}>
+        <Box movies={movies}>
           <MovieLists movies={movies} />
-        </ListBox>
-        <WatchBox />
+        </Box>
+        <Box>
+          <>
+            <WatchSummary watched={watched} />
+            <WatchList watched={watched} />
+          </>
+        </Box>
       </Main>
     </>
   );
@@ -113,17 +119,15 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && children}
+      {isOpen && children}
     </div>
   );
 }
@@ -150,28 +154,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-function WatchBox() {
-  const [isOpen2, setIsOpen2] = useState(true);
-  const [watched, setWatched] = useState(tempWatchedData);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchSummary watched={watched} />
-          <WatchList watched={watched} />
-        </>
-      )}
-    </div>
   );
 }
 
