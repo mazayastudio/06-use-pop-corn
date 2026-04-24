94d47a7150a53a801f13eb973d5aa29052f7f692
sayudhalw <mazaya.studio@outlook.com>
2026-04-24 08:28:04 +0700

    Enhance StarRating component: add prop-types, customizable styles, messages, and default rating support. Integrate updated component into main render.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={24} color="#00ffff" defaultRating={3} />
     <StarRating defaultValue={3} />
   </React.StrictMode>,
 );
